c3d0f7273f9baba5a51be5c924fb520c395e5dc9
isaacg11 <isaacgreywebdeveloper@gmail.com>
2017-10-02 13:28:13 -0700

    sending and recieving messages successfully

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
+let twilio = require('twilio');
+let accountSid = 'ACd8e8c7de910dffeb53cb2392df840932';
+let authToken = '939af02287fe8f073d41d6af29eb07ed';
+let client = new twilio(accountSid, authToken);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -8,9 +12,15 @@ app.get('/', (req, res) => {
   res.end();
 })
 
-app.post('/inbound', (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.post('/inbound', (req, res) => {  
+
+  client.messages.create({
+    body: 'Hello back',
+    to: `${req.body.From}`,
+    from: `${req.body.To}`
+  })
+
+  res.end();
 })
 
 app.listen(3000, () => {
